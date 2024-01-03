ca1893fda1970b88396e161b44dde5d81f4d8a78
MarceloALMoreira <marceloaomoreira@hotmail.com>
2024-01-03 02:08:39 -0300

    feat: add header and cart

--- index.php
+++ index.php
@@ -2,4 +2,32 @@
 ob_start();
 require('./sheep_core/config.php');
 ?>
-<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Mine Loja</title>
+    <link rel="stylesheet" href="assets/css/styles.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+
+</head>
+<body>
+    <!-- Topo do site -->
+    <div class="header">
+    <div class="logo">DB Soluções</div>
+        <div class="cart">
+            <i class="fa fa-shopping-cart"></i> 
+            <p>0</p>
+        </div>
+    </div>
+
+    <!-- Fim do site -->
+    <div class="footer">
+
+
+    </div>
+
+</body>
+</html>
